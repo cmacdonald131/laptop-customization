1b269ad8c781942f6350dcb75c9cbd7babd22584
Christopher MacDonald <55043368+cmacdonald131@users.noreply.github.com>
2019-12-30 21:15:45 -0500

    Add files via upload

---
+++ img/Cart.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+
+export default class Cart extends React.Component{
+    render() {
+      const summary = Object.keys(this.props.selected).map((feature, idx) => {
+      return(
+        <CartItem feature={feature}
+        idx={idx}
+        selected={this.props.selected} />
+      )  
+  
+        
+      });
+      const total = Object.keys(this.props.selected).reduce(
+        (acc, curr) => acc + this.props.selected[curr].cost,
+        0
+      );
+        return (
+        <section className="main__summary">
+            <h2>Your cart</h2>
+            {summary}
+            <div className="summary__total">
+              <div className="summary__total__label">Total</div>
+              <div className="summary__total__value">
+                {USCurrencyFormat.format(total)}
+              </div>
+            </div>
+          </section>
+        )
+    }
+}
